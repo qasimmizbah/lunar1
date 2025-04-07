19859aa452411c1aae82850a1c64a8af6001e119
qasim <qasimmizbah@gmail.com>
2025-04-07 17:49:25 +0530

    API

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+
+
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
@@ -18,5 +22,10 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/password', 'settings.password')->name('settings.password');
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
+
+Route::get('/api/categories', [CategoryController::class, 'index']);
+Route::get('/api/products', [ProductController::class, 'index']);
+Route::get('/api/products/{slug}', [ProductController::class, 'show']);
+Route::get('/api/categories/{slug}/products', [ProductController::class, 'productsByCategorySlug']);
 
 require __DIR__.'/auth.php';
